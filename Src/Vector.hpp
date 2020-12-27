4ebb6582add5a3f44b06c9db909c4e1fd0354ed2
Łukasz Czyż <krawacik3@gmail.com>
2020-12-27 01:26:49 +0100

    Added Doxygen documentation to Vector.hpp and VectorImpl.hpp

--- Src/Vector.hpp
+++ Src/Vector.hpp
@@ -4,12 +4,34 @@
 #include <VectorImpl.hpp>
 
 
+/**
+ * @brief Implementation of 3D Vector
+ * @tparam TYPE Underlying type that is used to store data and perform calculation. Only float and double are supported.
+ */
 template<typename TYPE>
 struct Vector3 : VectorImpl<3, TYPE>{
+    /**
+     * @brief Default constructor for null-vector
+     */
     Vector3();
+    /**
+     * @brief Constructor for setting all members
+     * @param x X
+     * @param y Y
+     * @param z Z
+     */
     Vector3(TYPE x, TYPE y, TYPE z);
+    /**
+     * @brief Copy constructor
+     * @param vector Vector to be copied
+     */
     Vector3(const VectorImpl<3, TYPE>& vector);
 
+    /**
+     * Getters for each parameter
+     * @return Reference to given member
+     * @{
+     */
     TYPE& x();
     TYPE& y();
     TYPE& z();
@@ -17,14 +39,38 @@ struct Vector3 : VectorImpl<3, TYPE>{
     const TYPE& x() const;
     const TYPE& y() const;
     const TYPE& z() const;
+    /** @} */
 };
 
+/**
+ * @brief Implementation of 4D Vector
+ * @tparam TYPE Underlying type that is used to store data and perform calculation. Only float and double are supported.
+ */
 template<typename TYPE>
 struct Vector4 : VectorImpl<4, TYPE>{
+    /**
+    * @brief Default constructor for null-vector
+    */
     Vector4();
+     /**
+      * @brief Constructor for setting all members
+      * @param a A
+      * @param b B
+      * @param c C
+      * @param d D
+      */
     Vector4(TYPE a, TYPE b, TYPE c, TYPE d);
+    /**
+     * @brief Copy constructor
+     * @param vector Vector to be copied
+     */
     Vector4(const VectorImpl<4, TYPE>& vector);
 
+    /**
+    * Getters for each parameter
+    * @return Reference to given member
+    * @{
+    */
     TYPE& a();
     TYPE& b();
     TYPE& c();
@@ -34,6 +80,7 @@ struct Vector4 : VectorImpl<4, TYPE>{
     const TYPE& b() const;
     const TYPE& c() const;
     const TYPE& d() const;
+    /** @} */
 };
 
 
--- Src/VectorImpl.hpp
+++ Src/VectorImpl.hpp
@@ -9,25 +9,67 @@
 template<typename TYPE>
 struct Quaternion;
 
+/**
+ * @brief Implementation of Vectors
+ * @tparam DIM Length of a vector
+ * @tparam TYPE Underlying type that is used to store data and perform calculation. Only float and double are supported.
+ */
 template<unsigned int DIM, typename TYPE>
 struct VectorImpl {
+    /**
+     * @brief Default constructor that creates null-vector
+     */
     VectorImpl();
 
-    // Defined in .hpp to avoid mess with template specialization
+    /**
+     * @brief Constructor that initializes vector's members
+     * @tparam T Type of elements passed as arguments
+     * @param initialValues Values of each vector's member starting from a first element
+     */
     template<typename... T>
     VectorImpl(T... initialValues) : values{initialValues...}{};
+
+    /**
+     * @brief Copy constructor
+     * @param other constructor to be copied
+     */
     VectorImpl(const VectorImpl<DIM, TYPE>& other) = default;
 
+    /**
+     * @brief Calculate length of a vector
+     * @return Length of a vector
+     */
     TYPE length() const;
+    /**
+     * @brief Calculate normalized vector based on current vector
+     * @return Vector of length equal to 1 or std::nullopt if length of current vector is equal to 0
+     */
     std::optional<VectorImpl<DIM, TYPE>> normalized() const;
+    /**
+     * @brief Rotate vector by a quaternion
+     * @param quaternion Quaternion used to rotate current vector
+     * @return New vector that is rotated by quaternion
+     * @see Quaternion
+     */
     VectorImpl<DIM, TYPE> rotatedByQuaternion(const Quaternion<TYPE>& quaternion) const;
 
-    // Operators
+    /**
+     * @brief Vector operators
+     * @return Calculated vector
+     * @{
+     */
     VectorImpl<DIM, TYPE> operator/(TYPE other) const;
     VectorImpl<DIM, TYPE> operator*(TYPE other) const;
     VectorImpl<DIM, TYPE> operator+(const VectorImpl<DIM, TYPE>& other) const;
     VectorImpl<DIM, TYPE> operator-(const VectorImpl<DIM, TYPE>& other) const;
     VectorImpl<DIM, TYPE> operator-() const;
+    /** @} */
+
+    /**
+     * @brief Comparison operator
+     * @param other Vector to be compared to
+     * @return True if vectors are identical
+     */
     bool operator == (const VectorImpl<DIM, TYPE>& other) const;
 
     std::array<TYPE, DIM> values;
